d38d826deac1eed197445a93b85f3175ab3e9694
Paul Luneau <paul.luneau@mail.com>
2025-07-16 18:46:50 +0200

    Updated the application so that it can now draw an SDF on screen

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,18 +1,89 @@
 #include "Application.h"
 
+#include <imgui.h>
+#include <numeric>
+
+#include "SDFs/AllSDFs.h"
+#include "SDFs/Primitives/Circle.h"
+
 Application::Application(sf::RenderWindow &window) :
-    m_window(window)
+    m_window(window),
+    m_canvas(window.getSize()),
+    m_renderTexture(window.getSize()),
+	m_sceneSDF(std::move(CreateSceneSDF()))
 {
+    m_renderTexture.setSmooth(true);
+	ComputeSDF();
 }
 
 void Application::Update(const sf::Time &deltaTime)
 {
+	float deltaTimeSeconds = deltaTime.asSeconds();
+	m_currentDTIndex++;
+	if (m_currentDTIndex >= m_deltaTimes.size()) {
+		m_currentDTIndex = 0;
+	}
+	m_deltaTimes[m_currentDTIndex] = deltaTimeSeconds;
+
+	ComputeSDF();
 }
 
 void Application::Draw()
 {
+	m_canvas.DrawOnTexture(m_renderTexture);
+
+    sf::Sprite sprite(m_renderTexture.getTexture());
+    const sf::FloatRect bounds = sprite.getGlobalBounds();
+    const sf::Vector2f origin = bounds.size * 0.5f;
+    sprite.setOrigin(origin);
+    sprite.setPosition(origin);
+
+    m_window.draw(sprite);
 }
 
 void Application::DrawImGui(const sf::Time &deltaTime)
 {
+	// General stats
+	float deltaTimeSeconds = deltaTime.asSeconds();
+	if (ImGui::CollapsingHeader("General statistics", ImGuiTreeNodeFlags_::ImGuiTreeNodeFlags_DefaultOpen)) 
+	{
+		float averageDeltaTime = std::accumulate(m_deltaTimes.begin(), m_deltaTimes.end(), 0.0f) / m_deltaTimes.size();
+		ImGui::Value("Update Time		", deltaTimeSeconds, "%0.6f");
+		ImGui::Value("FPS				", 1.0 / deltaTimeSeconds, "%0.6f");
+
+		if (m_currentDTIndex == 0) {
+			m_captureAverageDeltaTime = averageDeltaTime;
+		}
+		ImGui::Value("Avg Update Time	", m_captureAverageDeltaTime, "%0.6f");
+		ImGui::Value("Avg FPS			", 1.0f / m_captureAverageDeltaTime, "%0.6f");
+	}
+
+	// Scene SDF
+	if (ImGui::CollapsingHeader("Scene settings", ImGuiTreeNodeFlags_::ImGuiTreeNodeFlags_DefaultOpen)) 
+	{
+		int id = 0;
+		m_sceneSDF->DrawDebug(id);
+	}
+}
+
+void Application::ComputeSDF()
+{
+	const sf::Vector2u size = m_window.getSize();
+	for (uint32_t x = 0u; x < size.x; ++x) 
+	{
+		for (uint32_t y = 0u; y < size.y; ++y)
+		{
+			FloatType distance = m_sceneSDF->Evaluate({ static_cast<FloatType>(x), static_cast<FloatType>(y) });
+			m_canvas.SetPointColor(x, y, distance < 0.0f ? sf::Color::White : sf::Color::Black);
+		}
+	}
+}
+
+SDF_Ptr<FloatType> Application::CreateSceneSDF()
+{
+	SDF_Ptr<FloatType> circle0 = std::make_unique<Circle<FloatType>>(sf::Vector2<FloatType>{200.0f, 100.0f}, 200.0f);
+	SDF_Ptr<FloatType> circle1 = std::make_unique<Circle<FloatType>>(sf::Vector2<FloatType>{500.0f, 700.0f}, 100.0f);
+
+	SDF_Ptr<FloatType> operation = std::make_unique<MinOperator<FloatType>>(std::move(circle0), std::move(circle1));
+	return operation;
 }
--- src/Application.h
+++ src/Application.h
@@ -1,10 +1,30 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <array>
+#include <memory>
+
+#include "Canvas.h"
+#include "SDFs/SDF.h"
+
+#include "Alias.h"
+
+using namespace Alias;
+using namespace Configuration;
 
 class Application
 {
 private:
     sf::RenderWindow& m_window;
+
+    Canvas m_canvas;
+    sf::RenderTexture m_renderTexture;
+
+    SDF_Ptr<Configuration::FloatType> m_sceneSDF;
+
+
+    std::array<double, 60> m_deltaTimes;
+    int m_currentDTIndex = 0;
+    float m_captureAverageDeltaTime;
 
 public:
     Application(sf::RenderWindow& window);
@@ -12,4 +32,8 @@ public:
     void Update(const sf::Time& deltaTime);
     void Draw();
     void DrawImGui(const sf::Time& deltaTime);
+
+private:
+    void ComputeSDF();
+    SDF_Ptr<Configuration::FloatType> CreateSceneSDF();
 };
