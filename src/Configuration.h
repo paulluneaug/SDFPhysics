25d041331b7c8b2b527a1a0bdde0c6c0747cbe13
Paul Luneau <paul.luneau@mail.com>
2025-07-16 18:43:28 +0200

    Moved the float type parameter in the configuration file

--- src/Configuration.h
+++ src/Configuration.h
@@ -3,7 +3,9 @@
 
 namespace Configuration
 {
-	const uint32_t MAX_FRAMERATE = 144u;
+	using FloatType = float;
+
+	const uint32_t MAX_FRAMERATE = 1000000u;
 
 	const sf::Vector2u WINDOW_SIZE = sf::Vector2u(1920u, 1080u);
 };
