c020fe51b06cd52e1eebaef34b573576e2acdaee
Paul Luneau <paul.luneau@mail.com>
2025-07-16 18:46:07 +0200

    Forgotten file

--- src/SDFs/SDF.h
+++ src/SDFs/SDF.h
@@ -1,8 +1,34 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <imgui.h>
 
 template<typename TFloatType>
 class SDF
 {
-    TFloatType Evaluate(const sf::Vector2<TFloatType>& position) const = 0;
+
+public:
+
+    virtual TFloatType Evaluate(const sf::Vector2<TFloatType>& samplePosition) const 
+    {
+        return Evaluate_Impl(samplePosition);
+    }
+
+    void DrawDebug(int& elementID)
+    {
+        ImGui::PushID(elementID++);
+        if (DisplayTreeNode())
+        {
+            DrawDebug_Impl(elementID);
+            ImGui::TreePop();
+        }
+        ImGui::PopID();
+    }
+
+protected:
+    virtual TFloatType Evaluate_Impl(const sf::Vector2<TFloatType>& samplePosition) const = 0;
+
+    virtual bool DisplayTreeNode() const = 0;
+    virtual void DrawDebug_Impl(int& elementID)
+    {
+    }
 };
