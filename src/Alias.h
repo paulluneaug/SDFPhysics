9484028e1839b43787cfd41b73d76f2fb7c0ca70
p.luneau <paul.luneau@mail.com>
2025-08-27 18:55:46 +0200

    More aliases

--- src/Alias.h
+++ src/Alias.h
@@ -3,10 +3,21 @@
 
 #include "Configuration.h"
 #include "SDFs/SDF.h"
+#include "GFs/GF.h"
 
 namespace Alias 
 {
 	template<typename TFloatType>
-	using SDF_Ptr = std::unique_ptr<SDF<TFloatType>>;
+	using SDF_UPtr = std::unique_ptr<SDF<TFloatType>>;
+
+	template<typename TFloatType>
+	using SDF_WPtr = std::weak_ptr<SDF<TFloatType>>;
+
+	template<typename TFloatType>
+	using SDF_RPtr = SDF<TFloatType>*;
+
+	
+	template<typename TFloatType>
+	using GF_UPtr = std::unique_ptr<GF<TFloatType>>;
 }
 
